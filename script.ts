6e5e96e225a8e0ca7c26b939f74e2f4f5aa47456
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-07-24 23:26:28 +0530

    Add files via upload
    first typescript code

---
+++ script.ts
@@ -0,0 +1,61 @@
+// Typescript 
+// types 
+// primitives--------- reference
+
+
+// number boolean undefinnde string charchter  
+
+// objects classes funtion arrys and tupless
+
+
+
+ let a =12;
+ let b = a;
+  a= a+2;
+
+
+let arr =[1,2,3,4];
+let arr2=arr;
+
+   arr2.pop();
+
+
+
+
+
+
+
+
+// there is no differences in js and ts only ther is add some new feature in ts
+
+let a:number;
+
+let arr :[];
+ let arr:sting ;
+
+let arr2:[boolean,number,string]=[false,1,"shiv"]
+
+
+
+let variable :unknown
+
+// // variable"hey"
+
+// 111111111-----------------1111111111111
+
+
+
+// basic types 
+//  number sting boolen 
+//  arrys tuples 
+//  any unkown never void 
+//  enums
+
+
+enum Direction{
+    up ="up",
+    left="left",
+    right="right",
+    down="down"
+}
+
